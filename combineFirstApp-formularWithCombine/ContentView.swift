713ad8eb635f0313fcaebe543d530538aa7eb5eb
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-04-18 13:45:28 +0000

    commit

--- combineFirstApp-formularWithCombine/ContentView.swift
+++ combineFirstApp-formularWithCombine/ContentView.swift
@@ -6,18 +6,47 @@
 //
 
 import SwiftUI
+import Combine
+
+class User : ObservableObject {
+    @Published var email : String = ""
+    @Published var password : String = ""
+}
 
 struct ContentView: View {
+    
+    @ObservedObject var userInfo = User()
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            VStack {
+                Form {
+                    Section(header: Text("Login Information")) {
+                        TextField("Email", text: $userInfo.email)
+                        SecureField("Password", text: $userInfo.password)
+                        Button("Login") {
+                                print("User Loged")
+                        }
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 1)
+                            .shadow(radius: 4)
+                            .padding(-1)
+                            .opacity(0.6)
+                    )
+                    Text("Email: \(userInfo.email)")
+                    Text("Password: \(userInfo.password)")
+                }
+            }
+            .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
