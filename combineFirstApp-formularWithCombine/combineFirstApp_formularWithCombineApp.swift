aaa1289b28852712084cc57407f3d7fc41a4c18a
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-04-18 11:57:06 +0000

    Initial Commit

---
+++ combineFirstApp-formularWithCombine/combineFirstApp_formularWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  combineFirstApp_formularWithCombineApp.swift
+//  combineFirstApp-formularWithCombine
+//
+//  Created by zakaria lachkar on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct combineFirstApp_formularWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ combineFirstApp-formularWithCombineUITests/combineFirstApp_formularWithCombineUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  combineFirstApp_formularWithCombineUITestsLaunchTests.swift
+//  combineFirstApp-formularWithCombineUITests
+//
+//  Created by zakaria lachkar on 18/04/2023.
+//
+
+import XCTest
+
+final class combineFirstApp_formularWithCombineUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
